a414bcca0d984c41b3f6313b0c05903f8e2b2d31
fleuraward <128103631+fleuraward@users.noreply.github.com>
2025-12-20 18:01:43 +0000

    loveberry migrate to github GO!!!

---
+++ .eleventy.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addGlobalData("permalink", "{{ page.filePathStem }}.html");
+
+  eleventyConfig.addPassthroughCopy("./src/assets/css/");
+  eleventyConfig.addWatchTarget("./src/assets/css/");
+  eleventyConfig.addPassthroughCopy("./src/assets/js/");
+  eleventyConfig.addWatchTarget("./src/assets/js/");
+
+  const filepath = path.join(__dirname, "src", "_data", "links.json");
+  
+  const stats = fs.statSync(filepath);
+  eleventyConfig.addGlobalData("linksLastModified", stats.mtime.toISOString());
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
